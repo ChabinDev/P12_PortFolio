9eb508ee84fa92dcf872e1ee0a64e6f02ab29ab4
ChabinDev <chabin_972@hotmail.fr>
2024-05-05 15:42:55 +0200

    Ajout du fichier DynamicText.jsx

--- src/components/DynamicText.jsx
+++ src/components/DynamicText.jsx
@@ -1,5 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-export const Footer = () => {
-  return <div>Footer</div>;
+const DynamicText = () => {
+  useEffect(() => {
+    const target = document.getElementById("text-target");
+    let array = [
+      "Sass",
+      "Css",
+      "HTML",
+      "JavaScript",
+      "React",
+      "MongoDB",
+      "NodeJS",
+      "ExpressJS",
+    ];
+    let wordIndex = 0;
+    let letterIndex = 0;
+
+    const createLetter = () => {
+      const letter = document.createElement("span");
+      target.appendChild(letter);
+
+      letter.classList.add("letter");
+      letter.style.opacity = "0";
+      letter.style.animation = "anim 5s ease forwards";
+      letter.textContent = array[wordIndex][letterIndex];
+
+      setTimeout(() => {
+        letter.remove();
+      }, 7000);
+    };
+
+    const loop = () => {
+      setTimeout(() => {
+        if (wordIndex >= array.length) {
+          wordIndex = 0;
+          letterIndex = 0;
+          loop();
+        } else if (letterIndex < array[wordIndex].length) {
+          createLetter();
+          letterIndex++;
+          loop();
+        } else {
+          letterIndex = 0;
+          wordIndex++;
+          setTimeout(loop, 8000);
+        }
+      }, 80);
+    };
+    loop();
+  }, []);
+
+  return (
+    <span className="dynamic-text">
+      <span>Freelance</span>
+      <span id="text-target"></span>
+    </span>
+  );
 };
+
+export default DynamicText;
