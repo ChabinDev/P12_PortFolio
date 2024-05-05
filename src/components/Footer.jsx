02ece2deaf96f00567654005730fab333e7c65a0
ChabinDev <chabin_972@hotmail.fr>
2024-05-05 19:11:32 +0200

    Published

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,61 +1,25 @@
-import React, { useEffect } from "react";
+import React from 'react'
 
-const DynamicText = () => {
-  useEffect(() => {
-    const target = document.getElementById("text-target");
-    let array = [
-      "Sass",
-      "Css",
-      "HTML",
-      "JavaScript",
-      "React",
-      "MongoDB",
-      "NodeJS",
-      "ExpressJS",
-    ];
-    let wordIndex = 0;
-    let letterIndex = 0;
+const Footer = () => {
 
-    const createLetter = () => {
-      const letter = document.createElement("span");
-      target.appendChild(letter);
+  const theme = 'Dark'
 
-      letter.classList.add("letter");
-      letter.style.opacity = "0";
-      letter.style.animation = "anim 5s ease forwards";
-      letter.textContent = array[wordIndex][letterIndex];
-
-      setTimeout(() => {
-        letter.remove();
-      }, 7000);
-    };
-
-    const loop = () => {
-      setTimeout(() => {
-        if (wordIndex >= array.length) {
-          wordIndex = 0;
-          letterIndex = 0;
-          loop();
-        } else if (letterIndex < array[wordIndex].length) {
-          createLetter();
-          letterIndex++;
-          loop();
-        } else {
-          letterIndex = 0;
-          wordIndex++;
-          setTimeout(loop, 8000);
-        }
-      }, 80);
-    };
-    loop();
-  }, []);
+  const year = new Date() 
 
   return (
-    <span className="dynamic-text">
-      <span>Freelance</span>
-      <span id="text-target"></span>
-    </span>
-  );
-};
+    <footer>
+    <div className='adress'>
+            <div className={`content '${theme}`} >
+              <h4>ChabinDev</h4>
+              <p>Nantes</p>
+              <p>Développeur Web Junior - Freelance</p>
+            </div>
+      </div>
+      <div className={`credits '${theme}`}>
+          <p>France - {year.getFullYear()} © - All Rights Reserved</p>
+      </div>
+    </footer>
+  )
+}
 
-export default DynamicText;
+export default Footer
