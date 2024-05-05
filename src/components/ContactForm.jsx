8fa1aa60120dfbae8f83cac3acb78086da1677a0
ChabinDev <chabin_972@hotmail.fr>
2024-05-05 15:41:54 +0200

    Ajout du fichier ContactForm.jsx

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,103 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import emailjs from "@emailjs/browser";
 
-export const Contact = () => {
+const ContactForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    const formMess = document.querySelector(".formMessage");
+
+    // Votre EmailJs service ID, Template ID et Public Key
+    const serviceId = "service_pu2nf79";
+    const templateId = "template_tesmtac";
+    const publicKey = "DcIo_T4gWNj5iiy-o";
+
+    // Création d'un nouvel Objet dynamic
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      to_name: "ChabinDev",
+      message: message,
+    };
+
+    emailjs.send(serviceId, templateId, templateParams, publicKey).then(
+      (response) => {
+        console.log("SUCCESS!");
+        setName("");
+        setEmail("");
+        setMessage("");
+        formMess.innerHTML = "<p id='success'>Message envoyé !</p>";
+
+        setTimeout(() => {
+          formMess.innerHTML = "";
+        }, 2500);
+      },
+      (error) => {
+        console.log("FAILED...", error.text);
+        formMess.innerHTML =
+          "<p id='error'>Une erreur s'est produite , veuillez réessayer</p>";
+
+        setTimeout(() => {
+          formMess.innerHTML = "";
+        }, 2500);
+      }
+    );
+  };
+
   return (
-    <section id="contact" className="relative">
-      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-          <iframe
-            width="100%"
-            height="100%"
-            title="map"
-            className="absolute inset-0"
-            frameBorder={0}
-            marginHeight={0}
-            marginWidth={0}
-            style={{ filter: "opacity(0.7)" }}
-            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d21679.474243916848!2d-1.523712!3d47.217868200000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sfr!4v1714135676957!5m2!1sfr!2sfr"
-            width="600"
-            height="450"
-            loading="lazy"
-          />
-          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-            <div className="lg:w-1/2 px-6">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                ADDRESS
-              </h2>
-              <p className="mt-1">
-                97 Warren St. <br />
-                New York, NY 10007
-              </p>
-            </div>
-            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                EMAIL
-              </h2>
-              <a className="text-indigo-400 leading-relaxed">
-                reedbarger@email.com
-              </a>
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
-                PHONE
-              </h2>
-              <p className="leading-relaxed">123-456-7890</p>
-            </div>
-          </div>
-        </div>
-        <form
-          netlify
-          name="contact"
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
-        >
-          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Hire Me
-          </h2>
-          <p className="leading-relaxed mb-5">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-            suscipit officia aspernatur veritatis. Asperiores, aliquid?
-          </p>
-          <div className="relative mb-4">
-            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-          <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-          <div className="relative mb-4">
-            <label
-              htmlFor="message"
-              className="leading-7 text-sm text-gray-400"
-            >
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-          <button
-            type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-          >
-            Submit
-          </button>
-        </form>
-      </div>
-    </section>
+    <div className="form-container">
+      <h2>Contactez-nous</h2>
+      <form onSubmit={sendEmail} className="form-content">
+        <label>Nom</label>
+        <input
+          type="text"
+          required
+          autoComplete="off"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label>Email</label>
+        <input
+          type="email"
+          required
+          autoComplete="off"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>Message</label>
+        <textarea
+          name="message"
+          placeholder="Ecrivez votre message ici"
+          required
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <input type="submit" value="Envoyer" className="hover button" />
+      </form>
+      <div className="formMessage"></div>
+    </div>
   );
 };
+
+export default ContactForm;
