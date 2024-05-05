73a4dffc056a422efa797131b1de25db17d5f79d
ChabinDev <chabin_972@hotmail.fr>
2024-05-05 17:45:42 +0200

    Published

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Project1 from "./pages/Project1";
@@ -11,10 +11,10 @@ import { AnimatePresence } from "framer-motion";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AnimatePresence>
         <Routes>
-          <Route path="/P12_PortFolio/" exact element={<Home />} />
+          <Route path="/" exact element={<Home />} />
           <Route path="/Projet-1" exact element={<Project1 />} />
           <Route path="/Projet-2" exact element={<Project2 />} />
           <Route path="/Projet-3" exact element={<Project3 />} />
@@ -24,7 +24,7 @@ function App() {
           <Route path="/*" element={<Navigate replace to="/404" />} />
         </Routes>
       </AnimatePresence>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
