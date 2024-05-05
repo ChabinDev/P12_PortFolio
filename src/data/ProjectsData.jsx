9499db335379a69a195ed26925176e6620402777
ChabinDev <chabin_972@hotmail.fr>
2024-05-05 15:37:01 +0200

    Ajout des images du Carousel

---
+++ src/data/ProjectsData.jsx
@@ -0,0 +1,42 @@
+export const projectsData = [
+  {
+    id: 1,
+    title: "Booki",
+    date: "Novembre 2023",
+    languages: ["Html", "Css"],
+    infos:
+      "Lâ€™entreprise souhaite dÃ©velopper un site Internet qui permette aux usagers de trouver   des hÃ©bergements et des activitÃ©s dans la ville de leur choix. Je suis chargÃ© d'intÃ©grer l'interface du site avec du code HTML et CSS.",
+    img: "./assets/img/booki/activites/florian-wehde.jpg",
+    link: "https://chabindev.github.io/P3_Booki/",
+  },
+  {
+    id: 2,
+    title: "Ohmyfood",
+    date: "DÃ©cembre 2023",
+    languages: ["Html", "Css", "Sass"],
+    infos:
+      "Ma mission est de dÃ©velopper un site â€œmobile firstâ€ qui rÃ©pertorie les menus de restaurants gastronomiques.",
+    img: "./assets/img/ohmyfood/restaurants/louis.jpg",
+    link: "https://chabindev.github.io/P4_Oh_my_Food/",
+  },
+  {
+    id: 3,
+    title: "Print it",
+    date: "DÃ©cembre 2023",
+    languages: ["Html", "Css", "JavaScript"],
+    infos:
+      "Dynamiser le site Internet statique dâ€™une petite imprimerie familiale nommÃ©e Print it.",
+    img: "./assets/img/print_it/yellow-book.jpg",
+    link: "https://chabindev.github.io/P5_Print_it/",
+  },
+  {
+    id: 4,
+    title: "Kasa",
+    date: "Mars 2024",
+    languages: ["React", "Css", "Sass", "JavaScript"],
+    infos:
+      "DÃ©velopper en Freelance pour Kasa, une entreprise de location d'appartements entre particuliers.",
+    img: "./assets/img/kasa/kalen.png",
+    link: "https://chabindev.github.io/P8_Kasa/",
+  },
+];
