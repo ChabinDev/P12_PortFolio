f7db8c6bd784338a2871033d24c7aa06d860d480
ChabinDev <chabin_972@hotmail.fr>
2024-05-05 17:34:57 +0200

    Published

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Project1 from "./pages/Project1";
@@ -15,12 +15,13 @@ function App() {
       <AnimatePresence>
         <Routes>
           <Route path="/P12_PortFolio/" exact element={<Home />} />
-          <Route path="/Projet-1" element={<Project1 />} />
-          <Route path="/Projet-2" element={<Project2 />} />
-          <Route path="/Projet-3" element={<Project3 />} />
-          <Route path="/Projet-4" element={<Project4 />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/*" element={<NotFound />} />
+          <Route path="/Projet-1" exact element={<Project1 />} />
+          <Route path="/Projet-2" exact element={<Project2 />} />
+          <Route path="/Projet-3" exact element={<Project3 />} />
+          <Route path="/Projet-4" exact element={<Project4 />} />
+          <Route path="/Contact" exact element={<Contact />} />
+          <Route path="/404" exact element={<NotFound />} />
+          <Route path="/*" element={<Navigate replace to="/404" />} />
         </Routes>
       </AnimatePresence>
     </BrowserRouter>
