f8a6bf59c1b8b5dc9458073904d2da4cff7c7326
ChabinDev <chabin_972@hotmail.fr>
2024-05-05 19:42:57 +0200

    Published

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
+  const theme = "Dark";
 
-  const theme = 'Dark'
-
-  const year = new Date() 
+  const year = new Date();
 
   return (
     <footer>
-    <div className='adress'>
-            <div className={`content '${theme}`} >
-              <h4>ChabinDev</h4>
-              <p>Nantes</p>
-              <p>Développeur Web Junior - Freelance</p>
-            </div>
+      <div className="adress">
+        <div className={`content '${theme}`}>
+          <h2>ChabinDev</h2>
+          <p>Nantes</p>
+          <p>Développeur Web Junior - Freelance</p>
+        </div>
       </div>
       <div className={`credits '${theme}`}>
-          <p>France - {year.getFullYear()} © - All Rights Reserved</p>
+        <p>France - {year.getFullYear()} © - All Rights Reserved</p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
