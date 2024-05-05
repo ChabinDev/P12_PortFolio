dc713b7b7fde13e4c9fce1be8cb03fb2bd30c5c2
ChabinDev <chabin_972@hotmail.fr>
2024-05-05 15:43:49 +0200

    Ajout du fichier Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-export const Navbar = () => {
+const DynamicText = () => {
+  useEffect(() => {
+    const target = document.getElementById("text-target");
+    let array = [
+      "Sass",
+      "Css",
+      "HTML",
+      "JavaScript",
+      "React",
+      "MongoDB",
+      "NodeJS",
+      "ExpressJS",
+    ];
+    let wordIndex = 0;
+    let letterIndex = 0;
+
+    const createLetter = () => {
+      const letter = document.createElement("span");
+      target.appendChild(letter);
+
+      letter.classList.add("letter");
+      letter.style.opacity = "0";
+      letter.style.animation = "anim 5s ease forwards";
+      letter.textContent = array[wordIndex][letterIndex];
+
+      setTimeout(() => {
+        letter.remove();
+      }, 7000);
+    };
+
+    const loop = () => {
+      setTimeout(() => {
+        if (wordIndex >= array.length) {
+          wordIndex = 0;
+          letterIndex = 0;
+          loop();
+        } else if (letterIndex < array[wordIndex].length) {
+          createLetter();
+          letterIndex++;
+          loop();
+        } else {
+          letterIndex = 0;
+          wordIndex++;
+          setTimeout(loop, 8000);
+        }
+      }, 80);
+    };
+    loop();
+  }, []);
+
   return (
-    <header className="bg-gray-800 md:sticky top-0 z-10">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
-            Reed Barger
-          </a>
-        </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
-            Past Work
-          </a>
-          <a href="#skills" className="mr-5 hover:text-white">
-            Skills
-          </a>
-          <a href="#testimonials" className="mr-5 hover:text-white">
-            Testimonials
-          </a>
-        </nav>
-        <a
-          href="#contact"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
-        >
-          Hire Me
-          <ArrowRightIcon className="w-4 h-4 ml-1" />
-        </a>
-      </div>
-    </header>
+    <span className="dynamic-text">
+      <span>Freelance</span>
+      <span id="text-target"></span>
+    </span>
   );
 };
+
+export default DynamicText;
