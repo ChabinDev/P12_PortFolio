3bb1664a735ccf4faea97d0a65e728a5e46d4a26
ChabinDev <chabin_972@hotmail.fr>
2024-05-05 15:45:34 +0200

    Ajout du Fichier Mouse.jsx

--- src/components/Mouse.jsx
+++ src/components/Mouse.jsx
@@ -1,33 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-export const Skills = () => {
-  return (
-    <section id="skills">
-      <div className="container px-5 py-10 mx-auto">
-        <div className="text-center mb-20">
-          <ChipIcon className="w-10 inline-block mb-4" />
-          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
-            Skills &amp; Technologies
-          </h1>
-          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
-            ipsa delectus eum quo voluptas aspernatur accusantium distinctio
-            possimus est.
-          </p>
-        </div>
-        <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-          {skills.map((skill) => (
-            <div key={skill} className="p-2 sm:w-1/2 w-full">
-              <div className="bg-gray-800 rounded flex p-4 h-full items-center">
-                <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
-                <span className="title-font font-medium text-white">
-                  {skill}
-                </span>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+const Mouse = () => {
+  useEffect(() => {
+    const cursor = document.querySelector(".cursor");
+    window.addEventListener("mousemove", (e) => {
+      cursor.style.top = e.y + "px";
+      cursor.style.left = e.x + "px";
+    });
+
+    document.querySelectorAll(".hover").forEach((link) => {
+      link.addEventListener("mouseover", () => {
+        cursor.classList.add("hovered");
+      });
+      link.addEventListener("mouseleave", () => {
+        cursor.style.transition = ".3s ease-out";
+        cursor.classList.remove("hovered");
+      });
+    });
+  }, []);
+  return <span className="cursor"></span>;
 };
+
+export default Mouse;
